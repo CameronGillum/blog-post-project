1f7a067f6d874cbde9bf1c2a2e98b3c3c9d61d49
CameronGillum <GillumTCameron@gmail.com>
2024-06-18 10:25:48 -0400

    Adds the theme slider

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,19 +2,20 @@
 const currentTheme = localStorage.getItem('theme');
 if (currentTheme) {
     document.body.classList.add(currentTheme);
+    document.getElementById('theme-slider').checked = currentTheme === 'dark-mode';
 } else {
     // If no theme preference is found, use browser preference (light mode)
     const preferredTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark-mode' : 'light-mode';
     document.body.classList.add(preferredTheme);
     localStorage.setItem('theme', preferredTheme);
+    document.getElementById('theme-slider').checked = preferredTheme === 'dark-mode';
 }
 
 // Toggle between light and dark mode
-document.getElementById('theme-toggle').addEventListener('click', function() {
-    const currentTheme = document.body.classList.contains('dark-mode') ? 'dark-mode' : 'light-mode';
-    const newTheme = currentTheme === 'dark-mode' ? 'light-mode' : 'dark-mode';
+document.getElementById('theme-slider').addEventListener('change', function() {
+    const newTheme = this.checked ? 'dark-mode' : 'light-mode';
 
-    document.body.classList.remove(currentTheme);
+    document.body.classList.remove('dark-mode', 'light-mode');
     document.body.classList.add(newTheme);
 
     localStorage.setItem('theme', newTheme);
